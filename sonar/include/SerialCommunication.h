1fc1e2f72fa192c3ac59bdd30f08e7066f6a065d
Boris_il_forte <Boris_il_forte@mail.it>
2012-02-27 16:22:13 +0100

    Aggiunte licenza a tutti i file sorgente
    aggiunta licenza gplv2 a tutti i file sorgente. da controllare se valida e da aggiungere citazione studenti su vecchio codice

--- sonar/include/SerialCommunication.h
+++ sonar/include/SerialCommunication.h
@@ -1,3 +1,20 @@
+/*
+ * RoboTower, Hi-CoRG based on ROS
+ *
+ *
+ * Copyright (C) 2011 Marcello Pogliani, Davide Tateo
+ * Versione 1.0
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
 #ifndef SERIAL_COMMUNICATION_H
 #define SERIAL_COMMUNICATION_H
 
