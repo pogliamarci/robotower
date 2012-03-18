6c97923446c8aa5207275903f44f777858283bf4
Boris_il_forte <Boris_il_forte@mail.it>
2012-03-18 21:41:40 +0100

    Risolto problema di compilazione su Foxtrot
    MAI definire le funzioni negli header! xD

--- Foxtrot/src/foxtype.cpp
+++ Foxtrot/src/foxtype.cpp
@@ -15,33 +15,29 @@
  * GNU General Public License for more details.
  */
 
-#include <iostream>
-#include <string>
-#include <cmath>
-#include <vector>
+#include "foxtype.h"
 
-#define POINTNUMBERS 4
-#define DIRECTIONS 4
-#define ANGLEINCREMENT 45
-#define ROBOTRADIUS 14.14213562
-
-class Point
+Point::Point(float x, float y)
 {
-	public:
-		Point(float x, float y);
-		float x;
-		float y;
-		Point operator+ (Point p);
-		Point& operator+= (const Point& p);
-};
+	this->x=x;
+	this->y=y;
+}
 
-class Color_rgb
+Point Point::operator+ (Point p)
 {
-	public:
-		Color_rgb(char r, char g, char b);
-		char r;
-		char g;
-		char b;
-		
-};
+	return Point(this->x+p.x, this->y+p.y);
+}
 
+Point& Point::operator+=(const Point& p)
+{
+            x += p.x ;
+            y += p.y ;
+            return *this ;
+}
+
+Color_rgb::Color_rgb(char r, char g, char b)
+{
+	this->r=r;
+	this->g=g;
+	this->b=b;
+}
